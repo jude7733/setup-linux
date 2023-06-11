50229afaf8e0116b81edf77443a60e15e1e32a5a
jude <judeputhussery360@gmail.com>
2023-06-11 19:34:09 +0530

    minor

--- apt.sh
+++ apt.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo -e "\v ---This script is for apt only---\v"
+
 softwares=("neofetch" "gdebi" "vlc" "libreoffice" "gimp" "software-properties-common" "apt-transport-https" "wget" "curl" "python3" "pip3" "pipenv" "thunderbird" "htop" "spotify-client" "code" "kodi" "timeshift" "telegram-desktop")
 
 for i in "${softwares[@]}"
 do
-    read -p "do you wanna install $i ?: " n
-    case $n in
+    read -p "Do you wanna install $i? [y/n]: " choice
+    case $choice in
         y|Y)
             echo -e "\v ---Installing $i---\v"
             sudo apt install $i
--- repositories.sh
+++ repositories.sh
@@ -5,7 +5,11 @@ sudo add-apt-repository multiverse
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+#code
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
 
 sudo add-apt-repository ppa:lutris-team/lutris
 
