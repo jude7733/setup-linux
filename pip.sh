486f9cfa21a7ba9abcd66cf5f2dd343788cf338d
jude <judeputhussery360@gmail.com>
2023-06-20 11:43:18 +0530

    updated pip script
    added choice (y/n)

---
+++ pip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+python3 -m pip install --upgrade pip
+
+echo -e "\v ---This script is for python/pip packages---\v"
+
+packages=("notebook" "numpy" "pandas" "matplotlib" "seaborn" "scikit-learn" "tk" "pygame" "pygame-menu")
+
+for i in "${packages[@]}"
+do
+    read -p "Do you wanna install $i? [y/n]: " choice
+    case $choice in
+        y|Y)
+            echo -e "\v ---Installing $i---\v"
+            pip install $i
+            ;;
+        n|N)
+            echo -e "\v ---Skipping $i---\v"
+            ;;
+        *)
+            echo "you failed to click y/n, skipping $i"
+            ;;
+    esac
+done
